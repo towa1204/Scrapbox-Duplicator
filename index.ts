3d4163d50d108a8d5b150caae13965e1f2f4dfa9
towa1204 <lsadsfj@gmail.com>
2023-06-19 22:38:00 +0900

    変数名などを修正

--- index.ts
+++ index.ts
@@ -1,29 +1,29 @@
 import { assertString, exportPages, importPages } from "./deps.ts";
 
-const fromsid = Deno.env.get("SOURCE_PROJECT_SID");
-const tosid = Deno.env.get("DESTINATION_PROJECT_SID");
-const exportingProjectName = Deno.env.get("SOURCE_PROJECT_NAME");;  //インポート元(本来はprivateプロジェクト)
-const importingProjectName = Deno.env.get("DESTINATION_PROJECT_NAME"); //インポート先(publicプロジェクト)
+const fromSID = Deno.env.get("SOURCE_PROJECT_SID");
+const toSID = Deno.env.get("DESTINATION_PROJECT_SID");
+const exportProjectName = Deno.env.get("SOURCE_PROJECT_NAME");
+const importProjectName = Deno.env.get("DESTINATION_PROJECT_NAME");
 const shouldDuplicateByDefault =
   Deno.env.get("SHOULD_DUPLICATE_BY_DEFAULT") === "True";
 
-assertString(fromsid);
-assertString(tosid);
-assertString(exportingProjectName);
-assertString(importingProjectName);
+assertString(fromSID);
+assertString(toSID);
+assertString(exportProjectName);
+assertString(importProjectName);
 
-console.log(`Exporting a json file from "/${exportingProjectName}"...`);
-const result = await exportPages(exportingProjectName, {
-  sid: fromsid,
+console.log(`Exporting a json file from "/${exportProjectName}"...`);
+const exportResult = await exportPages(exportProjectName, {
+  sid: fromSID,
   metadata: true,
 });
-if (!result.ok) {
+if (!exportResult.ok) {
   const error = new Error();
-  error.name = `${result.value.name} when exporting a json file`;
-  error.message = result.value.message;
+  error.name = `${exportResult.value.name} when exporting a json file`;
+  error.message = exportResult.value.message;
   throw error;
 }
-const { pages } = result.value;
+const { pages } = exportResult.value;
 console.log(`Export ${pages.length}pages:`);
 for (const page of pages) {
   console.log(`\t${page.title}`);
@@ -38,23 +38,23 @@ const importingPages = pages.filter(({ lines }) => {
     return shouldDuplicateByDefault;
   }
 });
-
 if (importingPages.length === 0) {
   console.log("No page to be imported found.");
-} else {
-  console.log(
-    `Importing ${importingPages.length} pages to "/${importingProjectName}"...`,
-  );
-  const result = await importPages(importingProjectName, {
-    pages: importingPages,
-  }, {
-    sid: tosid,
-  });
-  if (!result.ok) {
-    const error = new Error();
-    error.name = `${result.value.name} when importing pages`;
-    error.message = result.value.message;
-    throw error;
-  }
-  console.log(result.value);
+  Deno.exit();
 }
+
+console.log(
+  `Importing ${importingPages.length} pages to "/${importProjectName}"...`,
+);
+const importResult = await importPages(importProjectName, {
+  pages: importingPages,
+}, {
+  sid: toSID,
+});
+if (!importResult.ok) {
+  const error = new Error();
+  error.name = `${importResult.value.name} when importing pages`;
+  error.message = importResult.value.message;
+  throw error;
+}
+console.log(importResult.value);
